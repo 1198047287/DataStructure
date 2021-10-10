036775f6f3bbe3410499e05d2abd9201798dc9d9
allen.yang <1198047287@qq.com>
2021-10-10 18:44:19 +0800

    ss

--- src/com/yyj/ArrayList.java
+++ src/com/yyj/ArrayList.java
@@ -1,5 +1,7 @@
 package com.yyj;
 
+import java.util.Arrays;
+
 /**
  * 动态数组
  */
@@ -33,16 +35,36 @@ public class ArrayList {
     }
 
     public boolean contanis(int element) {
-        return false;
+        return indexOf(element)!=ELEMENT_NOT_FOUND;
     }
 
 
     public void add(int element) {
 
+        ensureCapacity(size+1);
+
+        elements[size] = element;
+        size++;
+    }
+
+    /**
+     * 扩容算法
+     * @param capacity
+     */
+    private void ensureCapacity(int capacity) {
+        int oldCapacity = elements.length;
+        if (capacity<oldCapacity)return;
+
+        int newCapacity = oldCapacity+oldCapacity>>1;
+        int[] newElements = new int[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newElements[i] = elements[i];
+        }
+        this.elements = newElements;
     }
 
     public int get(int index){
-        if (size==0||index>size){
+        if (size==0||index>=size){
             throw new IndexOutOfBoundsException("size: "+size+" index: "+index);
         }
         return elements[index];
@@ -50,7 +72,7 @@ public class ArrayList {
 
     public int set(int index,int element) {
         // 注意  思想  size即为我们自己控制的
-        if (size==0||index>size){
+        if (size==0||index>=size){
             throw new IndexOutOfBoundsException("size: "+size+" index: "+index);
         }
         int old = elements[index];
@@ -58,8 +80,60 @@ public class ArrayList {
         return old;
     }
 
+    /**
+     * 从该位置开始往后挪  .
+     *
+     * 前边思路错误。 改： 1。 要从最后一位开始挪动。   2。注意for循环条件  >=  而不是<
+     *
+     * @param index
+     * @param element
+     */
     public void add(int index,int element){
-
+        for (int i = size-1; i >= index; i--) {
+            elements[i+1] = elements[i];
+        }
+        elements[index] = element;
+        size++;
     }
 
+    // 删除索引位置的元素  后边的元素往前挪
+    public int remove(int index){
+        if (size==0||index>=size){
+            throw new IndexOutOfBoundsException("size: "+size+" index: "+index);
+        }
+        int element = elements[index];
+        for (int i = index; i <= size; i++) {
+            elements[i] = elements[i+1];
+        }
+        size--;
+        return element;
+    }
+
+    public int indexOf(int element){
+        for (int i = 0; i < elements.length; i++) {
+            if (elements[i]==element){
+                return i;
+            }
+        }
+        return ELEMENT_NOT_FOUND;
+    }
+
+    public void clear(){
+        // 注意 思想。  不用每次都把元素删除。  后边进行覆盖即可。
+         this.size = 0;
+    };
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[");
+        for (int i = 0; i < size; i++) {
+            stringBuilder.append(elements[i]);
+            if (i!=size-1){
+                stringBuilder.append(", ");
+            }
+        }
+        stringBuilder.append("]");
+        return stringBuilder.toString();
+    }
 }
--- src/com/yyj/Main.java
+++ src/com/yyj/Main.java
@@ -2,6 +2,14 @@ package com.yyj;
 
 public class Main {
     public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        list.add(99);
+        list.add(88);
+        list.add(77);
+
+        list.add(1,55);
+        System.out.println(list.toString());
+        System.out.println(list.size);
 
     }
 }
