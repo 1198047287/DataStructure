fc8cf8fb405f9e3e104e2d6714e62a86c543ad91
119804 <1198047287@qq.com>
2021-10-17 15:03:05 +0800

    demo1

---
+++ src/com/yyj/01_动态数组/ArrayList.java
@@ -0,0 +1,145 @@
+package com.yyj;
+
+import org.omg.CORBA.Object;
+
+import java.util.Arrays;
+
+/**
+ * 动态数组
+ */
+public class ArrayList<E> {
+    // 保存的元素
+    public E[] elements;
+    // 元素的数量
+    public int size;
+
+    public static final int DEAFUAL_SIZE = 10;
+    public static final int ELEMENT_NOT_FOUND = -1;
+
+    public ArrayList(){
+        this.elements = (E[]) new Object[DEAFUAL_SIZE];
+    }
+
+    /**
+     * 有参构造器ð
+     * @param capaticy
+     */
+    public ArrayList(int capaticy){
+        this.elements = (E[]) new Object[capaticy];
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size==0;
+    }
+
+    public boolean contanis(E element) {
+        return indexOf(element)!=ELEMENT_NOT_FOUND;
+    }
+
+
+    public void add(E element) {
+
+        ensureCapacity(size+1);
+
+        elements[size] = element;
+        size++;
+    }
+
+    /**
+     * 扩容算法
+     * @param capacity
+     */
+    private void ensureCapacity(int capacity) {
+        int oldCapacity = elements.length;
+        if (capacity<oldCapacity)return;
+
+        int newCapacity = (int) (oldCapacity*1.5);
+        E[] newElements = (E[]) new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newElements[i] = elements[i];
+        }
+        this.elements = newElements;
+        System.out.println(oldCapacity+"_扩容为_"+newCapacity);
+    }
+
+    public E get(int index){
+        if (size==0||index>=size){
+            throw new IndexOutOfBoundsException("size: "+size+" index: "+index);
+        }
+        return elements[index];
+    }
+
+    public E set(int index,E element) {
+        // 注意  思想  size即为我们自己控制的
+        if (size==0||index>=size){
+            throw new IndexOutOfBoundsException("size: "+size+" index: "+index);
+        }
+        E old = elements[index];
+        elements[index] = element;
+        return old;
+    }
+
+    /**
+     * 从该位置开始往后挪  .
+     *
+     * 前边思路错误。 改： 1。 要从最后一位开始挪动。   2。注意for循环条件  >=  而不是<
+     *
+     * @param index
+     * @param element
+     */
+    public void add(int index,E element){
+        for (int i = size-1; i >= index; i--) {
+            elements[i+1] = elements[i];
+        }
+        elements[index] = element;
+        size++;
+    }
+
+    // 删除索引位置的元素  后边的元素往前挪
+    public E remove(int index){
+        if (size==0||index>=size){
+            throw new IndexOutOfBoundsException("size: "+size+" index: "+index);
+        }
+        E element = elements[index];
+        for (int i = index; i <= size; i++) {
+            elements[i] = elements[i+1];
+        }
+        size--;
+        return element;
+    }
+
+    public int indexOf(E element){
+        for (int i = 0; i < elements.length; i++) {
+            if (elements[i]==element){
+                return i;
+            }
+        }
+        return ELEMENT_NOT_FOUND;
+    }
+
+    public void clear(){
+        // 注意 思想。  不用每次都把元素删除。  后边进行覆盖即可。 这是对于int来说
+        //this.size = 0;
+        for (int i = 0; i < elements.length; i++) {
+            elements[i] = null;
+        }
+    };
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[");
+        for (int i = 0; i < size; i++) {
+            stringBuilder.append(elements[i]);
+            if (i!=size-1){
+                stringBuilder.append(", ");
+            }
+        }
+        stringBuilder.append("]");
+        return stringBuilder.toString();
+    }
+}
---
+++ src/com/yyj/01_动态数组/Main.java
@@ -0,0 +1,23 @@
+package com.yyj;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        list.add(99);
+        list.add(88);
+        // ð
+        list.add(1,55);
+        System.out.println(list.toString());
+        System.out.println(list.size);
+        //TODO
+        // 思考： Person占用了8字节。   Cat占用了12字节。
+        // 所以： 对象数组，保存的是地址值。 而不是真实对象。
+        // 否则会不知道分配多少内存空间
+        Object[] objects = new Object[10];
+        objects[0] = new Person();
+        objects[1] = new Cat();
+
+
+
+    }
+}
