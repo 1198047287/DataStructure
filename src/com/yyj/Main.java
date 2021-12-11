7231ceadc10ba32996ba674f398b388da0a7d156
baize <1198047287@qq.com>
2021-12-11 17:59:32 +0800

    test

---
+++ src/com/yyj/Main.java
@@ -0,0 +1,24 @@
+package com.yyj;
+
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+        int status = 1;
+
+        switch (status){
+            case 1:
+                System.out.println("status = "+status);
+            case 2:
+                System.out.println("status = "+status);
+            case 3:
+                System.out.println("status = "+status);
+            case 4:
+                System.out.println("status = "+status);
+        }
+    }
+
+}
